a983de06d663a29ddb870b37789957ea2553533d
Nicola De Filippo <nicola@nicoladefilippo.it>
2024-03-02 17:32:18 +0100

    Initial Commit

---
+++ TableSortedApp/TableSortedAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TableSortedAppApp.swift
+//  TableSortedApp
+//
+//  Created by nicola de filippo on 02/03/24.
+//
+
+import SwiftUI
+
+@main
+struct TableSortedAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
