f05ab0fef8fcf9b517e19f5cbc6da2751fd6e67d
Nicola De Filippo <nicola@nicoladefilippo.it>
2024-03-02 17:34:20 +0100

    Table sorted

--- TableSortedApp/ContentView.swift
+++ TableSortedApp/ContentView.swift
@@ -7,15 +7,55 @@
 
 import SwiftUI
 
+
+struct Beer: Codable, Identifiable {
+    var id: Int
+    var name: String
+    var first_brewed: String
+    var image_url: String
+    var abv: Double
+}
+
+
 struct ContentView: View {
+    @State var beers: [Beer] = []
+    @State var page = 0
+    @State private var sortOrder = [KeyPathComparator(\Beer.name, order: .reverse)]
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Table(beers, sortOrder: $sortOrder) {
+            TableColumn("Name", value: \.name)
+            TableColumn("First Brew", value: \.first_brewed)
+            TableColumn("ABV") { beer in
+                Text("\(beer.abv)")
+            }
+            TableColumn("Image") { beer in
+                AsyncImage(url: URL(string: beer.image_url)) { image in
+                    image.resizable().scaledToFit()
+                } placeholder: {
+                    ProgressView()
+                }
+                .frame(width: 100, height: 100)
+                
+            }
+        }.onAppear {
+            Task {
+                await getBeers()
+            }
+        }.onChange(of: sortOrder) {
+            beers.sort(using: sortOrder)
+       }
+    }
+    func getBeers() async {
+        do {
+            page += 1
+            let url = URL(string: "https://api.punkapi.com/v2/beers?page=\(page)&per_page=30")!
+            let (data, _) = try await URLSession.shared.data(from: url)
+            let beersLoaded = try JSONDecoder().decode([Beer].self, from: data)
+            beers = beersLoaded + beers
+            beers.sort(using: sortOrder)
+        } catch {
+            print("Some error")
         }
-        .padding()
     }
 }
 
